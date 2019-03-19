d1b40d25d6e641e2eab2984285eaf8c2beea5c3d
CClarkes <connorclarkes@gmail.com>
2019-03-19 19:56:30 +0000

    Exercise 4 complete

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,8 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+s3 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+s4 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+s5 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+puts @mens_stores = Store.where(mens_apparel: true).order(name: :desc)
