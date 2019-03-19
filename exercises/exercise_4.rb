df823e66d3d396648ffd53e27a6c0c341962f704
CClarkes <connorclarkes@gmail.com>
2019-03-19 20:23:05 +0000

    Exercise 4 complete

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -11,4 +11,14 @@ s3 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, w
 s4 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
 s5 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
-puts @mens_stores = Store.where(mens_apparel: true).order(name: :desc)
+@mens_stores = Store.where(mens_apparel: true)
+@womens_stores = Store.where(womens_apparel: true, annual_revenue: (0...1000000))
+
+
+@mens_stores.each do |store|
+    puts store[:name], store[:annual_revenue]
+end
+
+@womens_stores.each do |store|
+    puts store[:name], store[:annual_revenue]
+end
